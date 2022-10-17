e84738b9d0d26e96fdbc6609e27f54db139f0d18
Scott Goldthwaite <scott@goldthwaite.com>
2022-10-17 10:57:18 -0400

    Add files via upload

---
+++ private_credentials.py
@@ -0,0 +1,5 @@
+# Get credentials from Twilio entry in password manager
+TWILIO_ACCOUNT_SID = "AC79****"
+TWILIO_AUTH_TOKEN =  "251d****"
+TWILIO_PHONE =   "+1973221****"
+TO_PHONE =       "+1973819****"
